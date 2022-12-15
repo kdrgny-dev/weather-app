e4a938324ace6ed2975513c19bf5255ea2340568
Kadir Günay <42644031+kdrgny-dev@users.noreply.github.com>
2022-12-15 18:09:47 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,19 @@ import React from 'react'
 import Head from 'next/head'
 import Search from '../components/Search'
 import ChangeTheme from '../components/ChangeTheme'
+import Router from 'next/router'
 
 export default function Home() {
+
+  const [pageTitle, setPageTitle] = React.useState(true)
+
+  React.useEffect(() => {
+    Router.events.on('routeChangeStart', () => setPageTitle(false))
+
+    return () => {
+      Router.events.off('routeChangeStart', () => setPageTitle(false))
+    }
+  }, [])
 
   return (
     <div className="container mx-auto">
@@ -16,9 +27,9 @@ export default function Home() {
 
       <div className="flex flex-col items-center justify-center min-h-screen gap-20">
 
-        <div className="flex items-center text-3xl font-bold">
-          Basic Weather App
-        </div>
+        {pageTitle && <div className="flex items-center text-3xl font-bold">
+          Basic Weather App with NextJS
+        </div>}
 
         <Search placeholder={'Search City -> e.g : İstanbul'} />
       </div>
